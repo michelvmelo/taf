a6f6cffb84f391300f940fb6dbce54f4f214bc2d
Michel Melo <michelvmelo@gmail.com>
2013-06-27 18:42:30 -0300

    fdp
    como tu é chato meu cacete

--- Teste/src/cliente/ClienteMain.java
+++ Teste/src/cliente/ClienteMain.java
@@ -9,10 +9,13 @@ import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
 
+import navigators.smart.tom.ServiceProxy;
+
 public class ClienteMain {
     
     public static void main(String[] args) throws IOException {
         
+        ServiceProxy clienteProxy = new ServiceProxy(Integer.parseInt(args[0])); 
         
     }
     
